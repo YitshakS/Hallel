4c51ada6ca78bfb428ef9ed357a9956b271071ff
YitshakS <schlissel@gmail.com>
2022-06-03 02:48:25 +0300

    upload site

---
+++ button_submit.js
@@ -0,0 +1,22 @@
+document.querySelector("#button").addEventListener("click", () => {
+	if (document.querySelector("#myForm").checkValidity()) {
+		setTimeout(() => {
+			document.querySelector("#button").classList.add("onclic");
+			validate();
+		}, 250);
+	}
+});
+function validate() {
+	setTimeout(() => {
+		setTimeout(() => {
+			document.querySelector("#button").classList.remove("onclic");
+			document.querySelector("#button").classList.add("validate");
+			callback();
+		}, 450);
+	}, 2250);
+}
+function callback() {
+	setTimeout(() => {
+		document.querySelector("#button").classList.remove("validate");
+	}, 1250);
+}
